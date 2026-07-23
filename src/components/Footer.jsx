42fb2c5c333cfe32d37c441110ae5fda75f77108
CluelessXftw <lutfi.azlan5@gmail.com>
2026-07-23 13:42:03 +0800

    Minor cleanups

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Mail, Phone, MapPin, Instagram, Facebook, Youtube } from "lucide-react";
+import { Mail, Phone, MapPin, Instagram} from "lucide-react";
 
 export default function Footer() {
   return (
